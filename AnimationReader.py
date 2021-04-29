ccf109f9cdaafefb189331195993dfed200bbd2b
DESKTOP-T3794MB\Kulagin <sergeant.coolagin@gmail.com>
2021-04-29 20:56:59 +0300

    Update AnimationReader class.
    Rename to use CamelCase.
    Make __get_animation to get the animation. Print animationHeaderTuple and animationHeader in the function.

--- AnimationReader.py
+++ AnimationReader.py
@@ -1,9 +1,13 @@
 import bpy
 
 # ImportHelper is a helper class, defines filename and invoke() function which calls the file selector.
+import struct
+
 from bpy_extras.io_utils import ImportHelper
 from bpy.props import StringProperty, BoolProperty, EnumProperty
 from bpy.types import Operator
+
+from . import AnimationHeader0xC8
 
 
 class AnimationReader(Operator, ImportHelper):
@@ -20,17 +24,25 @@ class AnimationReader(Operator, ImportHelper):
     )
 
     def execute(self, context):
-        operation_status = self.read_animation_file(context, self.filepath)
+        operation_status = self.__read_animation_file(context, self.filepath)
         return operation_status
 
-    def read_animation_file(self, context, filepath = None):
+    def __read_animation_file(self, context, filepath = None):
         print("started read_file method")
 
         with open(filepath, 'rb') as animationFile:
             binaryData = animationFile.read()
 
-        # would normally load the binaryData here
-        print(len(binaryData))
+        self.__get_animation(binaryData)
 
         operation_status = {'FINISHED'}
         return operation_status
+
+    def __get_animation(self, binaryData):
+        # "< 2B H 24I" stands for little endian, 2 unsigned bytes, unsigned short, 24 unsigned 4-byte integers
+        struct_packer = struct.Struct("< 2B H 24I")
+        animation_header_tuple = struct_packer.unpack_from(binaryData)
+        animation_header = AnimationHeader0xC8.AnimationHeader0xC8(animation_header_tuple)
+
+        print(animation_header_tuple)
+        print(animation_header)
--- importMenu.py
+++ importMenu.py
@@ -2,21 +2,21 @@ import bpy
 
 # from . import animationReader
 from . import textReader
-from . import animationReader
+from . import AnimationReader
 
 
 # Only needed if you want to add into a dynamic menu
 def menu_func_import(self, context):
     self.layout.operator(textReader.TextReader.bl_idname, text= "Text Import Operator")
-    self.layout.operator(animationReader.AnimationReader.bl_idname, text="Import Tekken 7 Animation")
+    self.layout.operator(AnimationReader.AnimationReader.bl_idname, text= "Import Tekken 7 Animation")
 
 def register():
     bpy.utils.register_class(textReader.TextReader)
-    bpy.utils.register_class(animationReader.AnimationReader)
+    bpy.utils.register_class(AnimationReader.AnimationReader)
     bpy.types.TOPBAR_MT_file_import.append(menu_func_import)
 
 
 def unregister():
     bpy.utils.unregister_class(textReader.TextReader)
-    bpy.utils.unregister_class(animationReader.AnimationReader)
+    bpy.utils.unregister_class(AnimationReader.AnimationReader)
     bpy.types.TOPBAR_MT_file_import.remove(menu_func_import)
