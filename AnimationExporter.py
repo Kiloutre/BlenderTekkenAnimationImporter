631021d8953bdcff23c188c6c84c87d983618889
Kiloutre <kilowwwww@gmail.com>
2022-05-08 10:39:21 +0200

    Add hand anim/face anim export buttons

--- AnimationExporter.py
+++ AnimationExporter.py
@@ -6,7 +6,8 @@ from bpy.props import StringProperty
 from bpy.types import Operator
 from bpy_extras.io_utils import ExportHelper
 
-from . TekkenAnimHelper import TekkenAnimation, getAnimFrameFromBones
+from . TekkenAnimHelper import TekkenAnimation, getAnimFrameFromBones, getFaceAnimFrameFromBones, getLeftHandAnimFrameFromBones, getRightHandAnimFrameFromBones
+from . characterFaces import getCharacterFacePos
 
 def get_keyframes(obj):
     keyframes = []
@@ -51,7 +52,6 @@ class AnimationExporter(Operator, ExportHelper):
             self.report({'ERROR'}, "No valid armature selected")
             return {'FINISHED'}
         
-        #animLength = sce.frame_end
         animLength = get_keyframes(armature)[-1]
 
         frames = []
@@ -71,4 +71,133 @@ class AnimationExporter(Operator, ExportHelper):
             f.write(bytes(newAnimation.data))
             
         self.report({'INFO'}, "Animation successfuly exported")
+        return {'FINISHED'}
+    
+class FaceAnimationExporter(Operator, ExportHelper):
+    """This appears in the tooltip of the operator and in the generated docs"""
+    bl_idname = "animation_reader.export_face_animation"  # important since its how bpy.ops.animation_reader.read_animation is constructed
+    bl_label = "Export Facial Animation"
+
+    # ImportHelper mixin class uses this
+    filename_ext = ".f_bin"
+
+    filter_glob: StringProperty(
+        default = "*.f_bin",
+        options = {'HIDDEN'},
+    )
+
+    def execute(self, context):
+        sce = bpy.context.scene
+        armature = getSourceArmature(bpy.context)
+        
+        if armature == None:
+            self.report({'ERROR'}, "No valid armature selected")
+            return {'FINISHED'}
+        
+        animLength = get_keyframes(armature)[-1]
+
+        characterId = 0
+        face_base_pos = getCharacterFacePos(characterId)
+        
+        frames = []
+        for f in range(sce.frame_start, animLength+1):
+            sce.frame_set(f)
+            frames.append(getFaceAnimFrameFromBones(armature, face_base_pos))
+        
+        newAnimation = TekkenAnimation(type="face")
+        newAnimation.setLength(animLength)
+        newAnimation.recalculateSize()
+        
+        for f in range(animLength):
+            for fieldId, fieldValue in enumerate(frames[f]):
+                newAnimation.setField(fieldValue, f, fieldId)
+            
+        with open(self.filepath, "wb") as f:
+            f.write(bytes(newAnimation.data))
+            
+        self.report({'INFO'}, "Facial Animation successfuly exported")
+        return {'FINISHED'}
+    
+class LeftHandAnimationExporter(Operator, ExportHelper):
+    """This appears in the tooltip of the operator and in the generated docs"""
+    bl_idname = "animation_reader.export_lhand_animation"  # important since its how bpy.ops.animation_reader.read_animation is constructed
+    bl_label = "Export Left-Hand Animation"
+
+    # ImportHelper mixin class uses this
+    filename_ext = ".h_bin"
+
+    filter_glob: StringProperty(
+        default = "*.h_bin",
+        options = {'HIDDEN'},
+    )
+
+    def execute(self, context):
+        sce = bpy.context.scene
+        armature = getSourceArmature(bpy.context)
+        
+        if armature == None:
+            self.report({'ERROR'}, "No valid armature selected")
+            return {'FINISHED'}
+        
+        animLength = get_keyframes(armature)[-1]
+
+        frames = []
+        for f in range(sce.frame_start, animLength+1):
+            sce.frame_set(f)
+            frames.append(getLeftHandAnimFrameFromBones(armature))
+        
+        newAnimation = TekkenAnimation(type="hand")
+        newAnimation.setLength(animLength)
+        newAnimation.recalculateSize()
+        
+        for f in range(animLength):
+            for fieldId, fieldValue in enumerate(frames[f]):
+                newAnimation.setField(fieldValue, f, fieldId)
+            
+        with open(self.filepath, "wb") as f:
+            f.write(bytes(newAnimation.data))
+            
+        self.report({'INFO'}, "Left-Hand Animation successfuly exported")
+        return {'FINISHED'}
+    
+class RightHandAnimationExporter(Operator, ExportHelper):
+    """This appears in the tooltip of the operator and in the generated docs"""
+    bl_idname = "animation_reader.export_rhand_animation"  # important since its how bpy.ops.animation_reader.read_animation is constructed
+    bl_label = "Export Right-Hand Animation"
+
+    # ImportHelper mixin class uses this
+    filename_ext = ".h_bin"
+
+    filter_glob: StringProperty(
+        default = "*.h_bin",
+        options = {'HIDDEN'},
+    )
+
+    def execute(self, context):
+        sce = bpy.context.scene
+        armature = getSourceArmature(bpy.context)
+        
+        if armature == None:
+            self.report({'ERROR'}, "No valid armature selected")
+            return {'FINISHED'}
+        
+        animLength = get_keyframes(armature)[-1]
+
+        frames = []
+        for f in range(sce.frame_start, animLength+1):
+            sce.frame_set(f)
+            frames.append(getRightHandAnimFrameFromBones(armature))
+        
+        newAnimation = TekkenAnimation(type="hand")
+        newAnimation.setLength(animLength)
+        newAnimation.recalculateSize()
+        
+        for f in range(animLength):
+            for fieldId, fieldValue in enumerate(frames[f]):
+                newAnimation.setField(fieldValue, f, fieldId)
+            
+        with open(self.filepath, "wb") as f:
+            f.write(bytes(newAnimation.data))
+            
+        self.report({'INFO'}, "Right-Hand Animation successfuly exported")
         return {'FINISHED'}
--- exportMenu.py
+++ exportMenu.py
@@ -1,18 +1,27 @@
 import bpy
 
 # from . import animationReader
-from . import AnimationExporter
+from . AnimationExporter import AnimationExporter, FaceAnimationExporter, LeftHandAnimationExporter, RightHandAnimationExporter
 
 
 # Only needed if you want to add into a dynamic menu
 def menu_func_export(self, context):
-    self.layout.operator(AnimationExporter.AnimationExporter.bl_idname, text= "Export Tekken 7 Animation")
+    self.layout.operator(AnimationExporter.bl_idname, text= "Export Tekken 7 Animation")
+    self.layout.operator(FaceAnimationExporter.bl_idname, text= "Export Tekken 7 Face Animation")
+    self.layout.operator(LeftHandAnimationExporter.bl_idname, text= "Export Tekken 7 Left-Hand Animation")
+    self.layout.operator(RightHandAnimationExporter.bl_idname, text= "Export Tekken 7 Right-Hand Animation")
 
 def register():
-    bpy.utils.register_class(AnimationExporter.AnimationExporter)
+    bpy.utils.register_class(AnimationExporter)
+    bpy.utils.register_class(FaceAnimationExporter)
+    bpy.utils.register_class(LeftHandAnimationExporter)
+    bpy.utils.register_class(RightHandAnimationExporter)
     bpy.types.TOPBAR_MT_file_export.append(menu_func_export)
 
 
 def unregister():
-    bpy.utils.unregister_class(AnimationExporter.AnimationExporter)
+    bpy.utils.unregister_class(AnimationExporter)
+    bpy.utils.unregister_class(FaceAnimationExporter)
+    bpy.utils.unregister_class(LeftHandAnimationExporter)
+    bpy.utils.unregister_class(RightHandAnimationExporter)
     bpy.types.TOPBAR_MT_file_export.remove(menu_func_export)
